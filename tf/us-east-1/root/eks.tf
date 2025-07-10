cef78bae9e9b5407bbc19b3ab8604c4e2fbf7b3d
Rafał Lizoń <lizonr@gmail.com>
2025-07-10 16:59:26 +0200

    Add basic network (#2)

---
+++ tf/us-east-1/root/eks.tf
@@ -0,0 +1,27 @@
+locals {
+  cluster_name = "swish-play"
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "20.8.4"
+
+  cluster_name    = local.cluster_name
+  cluster_version = "1.30"
+
+  subnet_ids      = module.vpc.public_subnets
+  vpc_id          = module.vpc.vpc_id
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.micro"]
+      desired_size   = 1
+      min_size       = 1
+      max_size       = 1
+    }
+  }
+
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = false
+  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
+}
---
+++ tf/us-east-1/root/iam.tf
@@ -0,0 +1,65 @@
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
+  ]
+}
+
+resource "aws_iam_role" "github_actions" {
+  name = "github-actions-eks-deploy"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
+      },
+      Action = "sts:AssumeRoleWithWebIdentity",
+      Condition = {
+        StringLike = {
+          "token.actions.githubusercontent.com:sub" = "repo:RafaelloLollipop/Swish-test:*"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_policy" "github_actions_permissions" {
+  name        = "GitHubActionsEKSAccess"
+  description = "Permissions for GitHub Actions to access EKS"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "eks:DescribeCluster",
+          "eks:ListClusters"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:*",
+          "dynamodb:*"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "sts:GetCallerIdentity"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
